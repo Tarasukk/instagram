86526dfaf699965cea8f670c778eb05b27a58eaf
Tarasukk <relaxsonik@gmail.com>
2023-08-30 12:57:28 +0300

    improved comments functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,6 +18,13 @@ class CommentsController < ApplicationController
     if @comment.user == current_user
       @comment.destroy
     end
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.remove(
+          "post#{@comment.post_id}ModalComment#{@comment.id}"
+        )
+      end
+    end
   end
 
   private
